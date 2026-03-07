6551cef8ad92743b6814fd549d57d5903a6a2387
Yeider Adrian Mina <yeideradrian.noba@gmail.com>
2026-03-07 10:24:55 -0500

    pruebas unitarias

--- src/main/java/com/reactive/nexo/ReactiveSpringBootApplication.java
+++ src/main/java/com/reactive/nexo/ReactiveSpringBootApplication.java
@@ -2,10 +2,8 @@ package com.reactive.nexo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @SpringBootApplication
-@EnableR2dbcAuditing
 public class ReactiveSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReactiveSpringBootApplication.class, args);
--- src/main/java/com/reactive/nexo/formbuilder/repository/TemplateGroupAttrRepository.java
+++ src/main/java/com/reactive/nexo/formbuilder/repository/TemplateGroupAttrRepository.java
@@ -4,6 +4,7 @@ import com.reactive.nexo.formbuilder.entity.TemplateGroupAttr;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface TemplateGroupAttrRepository extends R2dbcRepository<TemplateGroupAttr, Long> {
